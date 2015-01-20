99fcc7a0b45a5bf0f4c067847d0f33ddf6335c79
anadahz <kojgelo@inbox.com>
2015-01-20 02:27:34 +0100

    Typos, script corrections

--- lepidopter-generic_build.sh
+++ lepidopter-generic_build.sh
@@ -4,12 +4,13 @@
 # Required Packages
 # binfmt-support qemu qemu-user-static debootstrap kpartx lvm2 dosfstools
 
-set -e # Exit on first error
-#set -x # Enable debugging
+#set -e # Exit on first error
+set -x # Enable debugging
 
 OONIPROBE_PATH="/root/ooni-probe"
 
-deb_mirror="http://cdn.debian.net/debian"
+deb_mirror="http://ftp.fi.debian.org/debian/"
+# Apt-Cacher NG caching
 #deb_local_mirror="http://localhost:3142/debian"
 
 bootsize="64M"
@@ -30,8 +31,8 @@ bootfs="${rootfs}/boot"
 
 mydate=`date +%Y%m%d`
 
-if [ "$deb_local_mirror" == "" ]; then
-  deb_local_mirror=$deb_mirror
+if [ "${deb_local_mirror}" == "" ]; then
+  deb_local_mirror=${deb_mirror}
 fi
 
 image=""
@@ -42,28 +43,28 @@ if [ $EUID -ne 0 ]; then
   exit 1
 fi
 
-if ! [ -b $device ]; then
-  echo "$device is not a block device"
+if ! [ -b ${device} ]; then
+  echo "${device} is not a block device"
   exit 1
 fi
 
-if [ "$device" == "" ]; then
+if [ "${device}" == "" ]; then
   echo "no block device given, just creating an image"
-  mkdir -p $buildenv
+  mkdir -p ${buildenv}
   image="${buildenv}/lepidopter_${deb_release}_${mydate}.img"
-  dd if=/dev/zero of=$image bs=1MB count=${deb_image_size}
-  device=`losetup -f --show $image`
-  echo "image $image created and mounted as $device"
+  dd if=/dev/zero of=${image} bs=1MB count=${deb_image_size}
+  device=`losetup -f --show ${image}`
+  echo "image ${image} created and mounted as ${device}"
 else
-  dd if=/dev/zero of=$device bs=512 count=1
+  dd if=/dev/zero of=${device} bs=512 count=1
 fi
 
-fdisk $device << EOF
+fdisk ${device} << EOF
 n
 p
 1
 
-+$bootsize
++${bootsize}
 t
 c
 n
@@ -75,9 +76,9 @@ w
 EOF
 
 
-if [ "$image" != "" ]; then
-  losetup -d $device
-  device=`kpartx -va $image | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
+if [ "${image}" != "" ]; then
+  losetup -d ${device}
+  device=`kpartx -va ${image} | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
   device="/dev/mapper/${device}"
   bootp=${device}p1
   rootp=${device}p2
@@ -86,7 +87,8 @@ else
     bootp=${device}p1
     rootp=${device}p2
     if ! [ -b ${bootp} ]; then
-      echo "uh, oh, something went wrong, can't find bootpartition neither as ${device}1 nor as ${device}p1, exiting."
+      echo "uh, oh, something went wrong, can't find bootpartition neither as
+      ${device}1 nor as ${device}p1, exiting."
       exit 1
     fi
   else
@@ -95,25 +97,31 @@ else
   fi
 fi
 
-mkfs.vfat $bootp
-mkfs.ext4 $rootp
+mkfs.vfat ${bootp}
+mkfs.ext4 ${rootp}
 
-mkdir -p $rootfs
+mkdir -p ${rootfs}
 
-mount $rootp $rootfs
+mount ${rootp} ${rootfs}
 
-cd $rootfs
+mkdir -p ${rootfs}/proc
+mkdir -p ${rootfs}/sys
+mkdir -p ${rootfs}/dev
+mkdir -p ${rootfs}/dev/pts
 
-debootstrap --foreign --arch armel $deb_release $rootfs $deb_local_mirror
+cd ${rootfs}
+
+debootstrap --foreign --arch armel ${deb_release} ${rootfs} ${deb_local_mirror}
 cp /usr/bin/qemu-arm-static usr/bin/
-LANG=C chroot $rootfs /debootstrap/debootstrap --second-stage
+LANG=C chroot ${rootfs} /debootstrap/debootstrap --second-stage
 
-mount $bootp $bootfs
+mount ${bootp} ${bootfs}
 
-echo "deb $deb_local_mirror $deb_release main contrib non-free
+echo "deb ${deb_local_mirror} ${deb_release} main contrib non-free
 " > etc/apt/sources.list
 
-echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait" > boot/cmdline.txt
+echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200
+console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait" > boot/cmdline.txt
 
 echo "proc            /proc           proc    defaults        0       0
 /dev/mmcblk0p1  /boot           vfat    defaults        0       0
@@ -128,56 +136,67 @@ auto eth0
 iface eth0 inet dhcp
 " > etc/network/interfaces
 
-echo "vchiq
-snd_bcm2835
-" >> etc/modules
+echo "vchiq" >> etc/modules
 
-echo "console-common	console-data/keymap/policy	select	Select keymap from full list
-console-common	console-data/keymap/full	select	de-latin1-nodeadkeys
-" > debconf.set
+#echo "console-common	console-data/keymap/policy	select	Select keymap from full list
+#console-common console-data/keymap/full	select	de-latin1-nodeadkeys
+#" > debconf.set
 
-echo "#!/bin/bash
+(
+cat <<'EOF'
+#!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-debconf-set-selections /debconf.set
-rm -f /debconf.set
+#debconf-set-selections /debconf.set
+#rm -f /debconf.set
 apt-get update
-apt-get -y install git-core binutils ca-certificates curl locales console-common ntp openssh-server less parted vim keyboard-configuration
-wget https://raw.github.com/Hexxeh/rpi-update/master/rpi-update -O /usr/bin/rpi-update
+apt-get -y install git-core binutils ca-certificates curl locales \
+console-common ntp openssh-server less parted keyboard-configuration
+
+curl https://raw.github.com/Hexxeh/rpi-update/master/rpi-update -o \
+/usr/bin/rpi-update
+
 chmod +x /usr/bin/rpi-update
 mkdir -p /lib/modules/3.1.9+
+touch /boot/start.elf
 SKIP_BACKUP=1 rpi-update
 
 # Too fast, causes segmentation fault
 sleep 5s
 
-wget -O /sbin/raspi-config https://github.com/anadahz/raspi-config_lepidopter/raw/master/raspi-config
+curl -o /sbin/raspi-config \
+https://github.com/anadahz/raspi-config_lepidopter/raw/master/raspi-config
+
 chmod +x /sbin/raspi-config
 
 # Building ooniprobe
-echo "deb http://deb.torproject.org/torproject.org wheezy main" >> /etc/apt/sources.list
+echo "deb http://deb.torproject.org/torproject.org/ wheezy main" \
+>> /etc/apt/sources.list
 gpg --keyserver keys.gnupg.net --keyserver-options timeout=120 --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
-apt-get -y install tor lsb-release sudo screen tcpdump libffi-dev
-git clone https://github.com/TheTorProject/ooni-probe.git ${OONIPROBE_PATH}
-cd ${OONIPROBE_PATH}
+apt-get update
+apt-get -y install lsb-release sudo screen tcpdump tor tor-geoipdb
+git clone https://github.com/TheTorProject/ooni-probe.git \"${OONIPROBE_PATH}\"
+cd \"${OONIPROBE_PATH}\"
 ./setup-dependencies.sh -p -y
 python setup.py install
 
-history -c
-echo \"root:${PASSWD}\" | chpasswd
-sed -i -e 's/KERNEL\!=\"eth\*|/KERNEL\!=\"/' /lib/udev/rules.d/75-persistent-net-generator.rules
+echo \"root:\"${PASSWD}\"\" | chpasswd
+sed -i -e 's/KERNEL\!=\"eth\*|/KERNEL\!=\"/' \
+/lib/udev/rules.d/75-persistent-net-generator.rules
 rm -f /etc/udev/rules.d/70-persistent-net.rules
-rm -f third-stage
-" > third-stage
+history -c
+EOF
+) > third-stage
 chmod +x third-stage
-LANG=C chroot $rootfs /third-stage
+LANG=C chroot ${rootfs} /third-stage
+rm -f third-stage
 
-echo "deb $deb_mirror $deb_release main contrib non-free
-" > etc/apt/sources.list
+#echo "deb ${deb_mirror} ${deb_release} main contrib non-free" \
+#> etc/apt/sources.list
 
-remove_ssh_host_keys"
-rm -f /etc/ssh/ssh_host_*_key*
-cat <<\RCL | tee /etc/rc.local
+# rc.local
+(
+cat <<'EOF'
 #!/bin/sh -e
 #
 # rc.local
@@ -193,15 +212,18 @@ cat <<\RCL | tee /etc/rc.local
 
 # Print the IP address
 _IP=$(hostname -I) || true
-if [ "$_IP" ]; then
-  printf "My IP address is %s\n" "$_IP"
+if [ "${_IP}" ]; then
+  printf "My IP address is %s\n" "${_IP}"
 fi
 
 exit 0
-RCL
-# Re-enables at first boot after regeneration of ssh host keys
-update-rc.d ssh disable
-cat <<\EOF > /etc/init.d/regenerate_ssh_host_keys
+EOF
+) > etc/rc.local
+
+
+# regenerate_ssh_host_keys
+(
+cat <<'EOF'
 #!/bin/sh
 ### BEGIN INIT INFO
 # Provides:          regenerate_ssh_host_keys
@@ -219,51 +241,68 @@ set -e
 
 case "$1" in
   start)
-    log_daemon_msg "Regenerating ssh host keys (in background)"
+    log_daemon_msg "Regenerating ssh host keys in background..."
     nohup sh -c "yes | ssh-keygen -q -N '' -t dsa -f \
-    /etc/ssh/ssh_host_dsa_key && \ yes | ssh-keygen -q -N '' -t rsa -f \
-    /etc/ssh/ssh_host_rsa_key && \ yes | ssh-keygen -q -N '' -t ecdsa -f \
-    /etc/ssh/ssh_host_ecdsa_key && \ update-rc.d ssh enable && sync && \
-    rm /etc/init.d/regenerate_ssh_host_keys && \
-    update-rc.d regenerate_ssh_host_keys remove && \
-    printf '\nfinished\n' && invoke-rc.d ssh start" > \
-    /var/log/regen_ssh_keys.log 2>&1 &
+     /etc/ssh/ssh_host_dsa_key && yes | ssh-keygen -q -N '' -t rsa -f \
+     /etc/ssh/ssh_host_rsa_key && yes | ssh-keygen -q -N '' -t ecdsa -f \
+     /etc/ssh/ssh_host_ecdsa_key && update-rc.d ssh enable && sync && \
+     rm /etc/init.d/regenerate_ssh_host_keys && \
+     update-rc.d regenerate_ssh_host_keys remove && \
+     printf '\nfinished\n' && invoke-rc.d ssh start" \
+     > /var/log/regen_ssh_keys.log 2>&1 &
     log_end_msg $?
     ;;
   *)
-    echo "Usage: $0 start" >&2
+    log_success_msg "Usage: $0 start" >&2
     exit 3
     ;;
 esac
 EOF
+) > etc/init.d/regenerate_ssh_host_keys
+
+(
+cat <<'EOF'
+#!/bin/bash
+# Re-enables at first boot after regeneration of ssh host keys
+update-rc.d ssh disable
+rm -f /etc/ssh/ssh_host_*_key*
 chmod +x /etc/init.d/regenerate_ssh_host_keys
 update-rc.d regenerate_ssh_host_keys start 2
-rm remove_ssh_host_keys
-" > remove_ssh_host_keys
+history -c
+EOF
+) > remove_ssh_host_keys
+
 chmod +x remove_ssh_host_keys
-LANG=C chroot $rootfs /remove_ssh_host_keys
+LANG=C chroot ${rootfs} /remove_ssh_host_keys
+rm -f remove_ssh_host_keys
 
-echo "#!/bin/bash
+(
+cat <<'EOF'
+#!/bin/bash
 apt-get clean
-printf '\nCleaning up...\n'
-
+printf "\nCleaning up...\n"
 sleep 4s;
-rm -f cleanup
-" > cleanup
+history -c
+EOF
+) > cleanup
+
 chmod +x cleanup
-LANG=C chroot $rootfs /cleanup
+LANG=C chroot ${rootfs} /cleanup
+rm -f cleanup
 
 cd
 
 sync
 sleep 15
 
-umount -l $bootp
-umount -l $rootp
+umount -l ${bootp}
+umount -l ${rootp}
+# Remove device mappings. Avoid running out of loop devices.
+dmsetup remove_all
 
-if [ "$image" != "" ]; then
-  kpartx -d $image
-  echo "created image $image"
+if [ "${image}" != "" ]; then
+  kpartx -d ${image}
+  echo "created image ${image}"
   echo "The root pass: ${PASSWD}"
 fi
 
