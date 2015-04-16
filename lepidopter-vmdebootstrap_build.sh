f1cd455fdad1cb6f27ed42a598a6f243060af469
anadahz <kojgelo@inbox.com>
2015-04-16 18:35:00 +0200

    Rollback to wheezy distribution
    Switched to Debian wheezy distribution after encountering issues with python
    packages: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=769106
    * Remove python packages and various dependencies that are being installed by
      ooniprobe setup script
    * Remove duplicated package curl
    * Add haveged package
    * Add lsb-release package required by ooniprobe setup-dependencies script

--- lepidopter-vmdebootstrap_build.sh
+++ lepidopter-vmdebootstrap_build.sh
@@ -2,15 +2,19 @@
 
 USER="lepidopter"
 PASSWD="lepidopter"
-DEB_RELEASE="jessie"
+DEB_RELEASE="wheezy"
 HOSTNAME_IMG="lepidopter"
-APT_MIRROR="http://ftp.no.debian.org/debian/"
+APT_MIRROR="http://http.debian.net/debian/"
+# apt-cache-ng
+#MIRROR="http://localhost:3142/debian"
+MIRROR="http://http.debian.net/debian/"
 
 sudo vmdebootstrap \
     --arch armel \
     --log lepidopter-build-`date +%Y%m%d`.log \
     --distribution ${DEB_RELEASE} \
     --apt-mirror ${APT_MIRROR} \
+    --mirror ${MIRROR} \
     --image lepidopter-`date +%Y%m%d`.img \
     --size 3900M \
     --bootsize 64M \
@@ -25,7 +29,6 @@ sudo vmdebootstrap \
     --foreign /usr/bin/qemu-arm-static \
     --package netbase \
     --package ntp \
-    --package iproute2 \
     --package less \
     --package openssh-server \
     --package screen \
@@ -33,24 +36,9 @@ sudo vmdebootstrap \
     --package binutils \
     --package ca-certificates \
     --package wget \
-    --package curl \
     --package kmod \
     --package curl \
-    --package python \
-    --package python-dev \
-    --package python-setuptools \
-    --package build-essential \
-    --package libdumbnet1 \
-    --package python-dumbnet \
-    --package python-libpcap \
-    --package tor \
-    --package tor-geoipdb \
-    --package libgeoip-dev \
-    --package libpcap0.8-dev \
-    --package libssl-dev \
-    --package libffi-dev \
-    --package libdumbnet-dev \
-    --package tcpdump \
-    --package python-pip \
+    --package haveged \
+    --package lsb-release \
     --configure-apt \
     --customize `pwd`/customize
