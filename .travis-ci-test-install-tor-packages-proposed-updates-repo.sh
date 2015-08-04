bc7df1dbd96e29ca444f764f59ad099c591c3e8b
anadahz <kojgelo@inbox.com>
2015-08-04 15:36:23 +0200

    Add main Tor deb repo

--- .travis-ci-test-install-tor-packages-proposed-updates-repo.sh
+++ .travis-ci-test-install-tor-packages-proposed-updates-repo.sh
@@ -72,6 +72,7 @@ fi
 echo "Running tests"
 echo "Environment: $(uname -a)"
 sudo apt-key adv --keyserver ${KEYSERVER} --recv-keys `expr substr ${REPO_KEY} 33 8`
+echo "deb ${TOR_DEB_REPO} ${VERSION} main" | sudo tee -a ${APT_REPO_LIST}
 echo "deb ${TOR_DEB_REPO} ${PROPOSED_UPDATES_VERSION} main" |
 sudo tee -a ${APT_REPO_LIST}
 sudo apt-get -qq update
