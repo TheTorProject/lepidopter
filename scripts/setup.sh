f61ced02a5c63c7990270786464802e21d8fece1
anadahz <kojgelo@inbox.com>
2015-09-14 13:50:41 +0200

    Fix: append sid repository to list, install qemu-utils stable version
    Do not install qemu-utils from sid Debian repository (Bug)
    Append the sid repository to the current repository list

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ set_apt_sources() {
     SUITE="$1"
     PIN_PRIORITY="$2"
     COMPONENTS="main"
-    cat <<EOF > /etc/apt/sources.list
+    cat <<EOF >> /etc/apt/sources.list
 # Repository: $SUITE
 deb $APT_MIRROR $SUITE $COMPONENTS
 deb-src $APT_MIRROR $SUITE $COMPONENTS
@@ -23,7 +23,9 @@ EOF
 set_apt_sources sid 50
 apt-get update
 
-apt-get install -y vmdebootstrap
+# Bug: Do not install qemu-utils via the sid repo
+ept-get install -y qemu-utils
+apt-get install -t sid -y vmdebootstrap
 
 cd $HOME
 git clone https://github.com/TheTorProject/lepidopter.git
