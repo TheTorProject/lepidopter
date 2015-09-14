0d97d9a6942a449dc24ab8418a3796a09a29d14d
anadahz <kojgelo@inbox.com>
2015-09-14 12:54:01 +0200

    Use separate ooniprobe config file to upload the reports
    * Modify upload reports script to use separate config file
    * Add oonireport config file
    * Correct ooniprobe config file (default options)

--- lepidopter-fh/opt/ooni/upload-reports.sh
+++ lepidopter-fh/opt/ooni/upload-reports.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$(date) uploading ooniprobe reports" >> /var/log/ooni/oonireport.log
-oonireport -f /etc/ooniprobe/ooniprobe.conf upload \
+oonireport -f /etc/ooniprobe/oonireport.conf upload \
     >> /var/log/ooni/oonireport.log
