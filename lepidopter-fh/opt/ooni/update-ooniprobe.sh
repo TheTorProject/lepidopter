4a9388613491b6751756eaf8a043ce81e09496c3
anadahz <kojgelo@inbox.com>
2015-11-12 22:02:53 +0100

    Fix update ooniprobe script
    Downgrade of twisted should happen after upgrading ooniprobe otherwise will get
    upgraded!

--- lepidopter-fh/opt/ooni/update-ooniprobe.sh
+++ lepidopter-fh/opt/ooni/update-ooniprobe.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 source /etc/ooniprobe/ooniconfig.sh
-# Hack: downgrade Twisted
-pip -q uninstall -y Twisted
-pip -q install 'Twisted>=12.2.0,<=14.0.0'
 
+# Update ooniprobe
 pip -q install ooniprobe --upgrade
+# Hack: downgrade Twisted
+pip -q install 'Twisted>=12.2.0,<=14.0.0'
+# Update ooniresources
 ooniresources --update-inputs
