b9f62e26de239f8875003001ad5b7fb4921ea6e6
anadahz <kojgelo@inbox.com>
2015-11-15 22:18:24 +0100

    Use variables instead of hardcoded file paths
    Retrieve OONI deck variable from a separate OONI deck configuration file

--- lepidopter-fh/opt/ooni/run-ooniprobe.sh
+++ lepidopter-fh/opt/ooni/run-ooniprobe.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 source /etc/ooniprobe/ooniconfig.sh
-cd /opt/ooni/reports/
-echo "$(date) running ooniprobe" >> /var/log/ooni/cronjobs.log
+source /etc/ooniprobe/oonideckconfig
+cd ${OONI_REPORTS}
+echo "$(date) running ooniprobe" >> ${OONI_CRONJOBS_LOG}
 
-if [ -z "$OONI_DECK" ]
-  then
+if [ -z "${OONI_DECK}" ]
+then
     /opt/ooni/update-deck.sh
     source /etc/ooniprobe/ooniconfig.sh
 fi
 
-flock -n /run/ooniprobe.daily.lock -c "ooniprobe -f $OONI_CONFIG \
-    -i $OONI_DECK" >> /var/log/ooni/cronjobs.log
+flock -n /run/ooniprobe.daily.lock -c \
+    "ooniprobe -f ${OONI_CONFIG} -i ${OONI_DECK}" >> ${OONI_CRONJOBS_LOG}
