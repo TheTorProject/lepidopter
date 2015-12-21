37842f824d79c58a266d03beb47febcdd7112c32
anadahz <kojgelo@inbox.com>
2015-12-21 23:09:36 +0100

    Indent lines for better readability
    As spotted by bassosimone

--- lepidopter-fh/cleanup.sh
+++ lepidopter-fh/cleanup.sh
@@ -13,7 +13,7 @@ echo "Removing documentation..." >&2
 find /usr/share/doc -depth -type f ! -name copyright|xargs rm || true
 find /usr/share/doc -empty|xargs rmdir || true
 rm -rf /usr/share/man /usr/share/groff /usr/share/info /usr/share/lintian \
-/usr/share/linda /var/cache/man
+    /usr/share/linda /var/cache/man
 
 # Keep only the English translation
 find /usr/share/locale -mindepth 1 -maxdepth 1 ! -name 'en' |xargs rm -r
