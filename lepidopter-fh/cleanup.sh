406bd042af16953f0ce0752382680de2b44f161b
anadahz <kojgelo@inbox.com>
2015-12-21 14:43:11 +0100

    No need for postint conditional

--- lepidopter-fh/cleanup.sh
+++ lepidopter-fh/cleanup.sh
@@ -9,14 +9,11 @@ apt-get autoclean
 find /var/log -type f -delete
 
 # Remove the same set of files and directories
-# in the project-config's postinst.
-if [ "$1" = "configure" ] && [ -z "$2" ]; then
-    echo "Removing documentation..." >&2
-    find /usr/share/doc -depth -type f ! -name copyright|xargs rm || true
-    find /usr/share/doc -empty|xargs rmdir || true
-    rm -rf /usr/share/man /usr/share/groff /usr/share/info /usr/share/lintian \
-    /usr/share/linda /var/cache/man
-fi
+echo "Removing documentation..." >&2
+find /usr/share/doc -depth -type f ! -name copyright|xargs rm || true
+find /usr/share/doc -empty|xargs rmdir || true
+rm -rf /usr/share/man /usr/share/groff /usr/share/info /usr/share/lintian \
+/usr/share/linda /var/cache/man
 
 # Keep only the English translation
 find /usr/share/locale -mindepth 1 -maxdepth 1 ! -name 'en' |xargs rm -r
