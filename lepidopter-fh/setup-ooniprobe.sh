db3bb116d93aafb5b20dcbcdb9d6faeda5de7c93
anadahz <kojgelo@inbox.com>
2015-12-21 01:38:28 +0100

    Dependencies required prior installing PTs

--- lepidopter-fh/setup-ooniprobe.sh
+++ lepidopter-fh/setup-ooniprobe.sh
@@ -14,19 +14,18 @@ apt-get update
 apt-get -y -t ${DEB_RELEASE}-backports install golang-go
 go version
 
-# Install pluggable transports and dependencies
-apt-get -y install libgmp-dev python-pip
-# Install obfsproxy and fteproxy
-pip install obfsproxy fteproxy
 # Build meek-client
 export GOPATH=$($mktmp -d)
 go get git.torproject.org/pluggable-transports/meek.git/meek-client
 cp $GOPATH/bin/meek-client /usr/local/bin/meek-client
 chmod +x /usr/local/bin/meek-client
 rm -rf $GOPATH
-# Install ooniprobe dependencies
+# Install ooniprobe and pluggable transports dependencies
 apt-get -y install openssl libssl-dev libyaml-dev libsqlite3-dev libffi-dev \
-libpcap0.8-dev libgeoip-dev libdumbnet-dev tor tor-geoipdb python-dev
+libpcap0.8-dev libgeoip-dev libdumbnet-dev tor tor-geoipdb python-dev \
+libgmp-dev python-pip
+# Install obfsproxy and fteproxy
+pip install obfsproxy fteproxy
 # Install ooniprobe
 pip install git+https://github.com/TheTorProject/ooni-probe.git
 history -c
