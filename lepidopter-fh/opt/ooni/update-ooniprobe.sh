953b68ea8fab8a11003d5dbb685be27bd6d005ae
anadahz <kojgelo@inbox.com>
2016-05-12 17:46:33 -0300

    Ensure python-cryptography is not installed
    Make sure that python-cryptography package is not installed in our
    system.

--- lepidopter-fh/opt/ooni/update-ooniprobe.sh
+++ lepidopter-fh/opt/ooni/update-ooniprobe.sh
@@ -2,6 +2,10 @@
 source /etc/ooniprobe/ooniconfig.sh
 trap die ERR
 
+# Make sure to remove python-cryptography package that intereferes with many
+# python packages and system upgrades
+apt-get -y -qq purge python-cryptography
+
 # Update ooniprobe
 pip -q install ooniprobe --upgrade
 # Update ooniresources
