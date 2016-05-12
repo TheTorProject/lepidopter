88558f21ef07852d29d822aa751d63c8f6f6ed1c
anadahz <kojgelo@inbox.com>
2016-05-12 17:20:12 -0300

    Fix first ooni-probe deck generation
    * Add an empty source file to keep track of the ooni-prebe deck.
    * Add the correct source PATH and move the test clause in the right
      section of the run-ooniprobe script.
    As spotted by @hellais.

--- lepidopter-fh/opt/ooni/run-ooniprobe.sh
+++ lepidopter-fh/opt/ooni/run-ooniprobe.sh
@@ -3,11 +3,12 @@ source /etc/ooniprobe/ooniconfig.sh
 source /etc/ooniprobe/oonideckconfig
 trap die ERR
 
-cd ${OONI_REPORTS}
-echo "$(date) running ooniprobe" >> ${OONI_CRONJOBS_LOG}
-
 if [ -z "${OONI_DECK}" ]; then
+    source /etc/ooniprobe/oonideckconfig
    /opt/ooni/update-deck.sh
 fi
+
+cd ${OONI_REPORTS}
+echo "$(date) running ooniprobe" >> ${OONI_CRONJOBS_LOG}
 
 ooniprobe -f ${OONI_CONFIG} -i ${OONI_DECK} >> ${OONI_CRONJOBS_LOG}
