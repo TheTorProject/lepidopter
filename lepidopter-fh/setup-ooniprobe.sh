b5a705f895a0b3496bb8ee8ecfe468e0653ad4a5
anadahz <kojgelo@inbox.com>
2016-05-19 02:17:43 -0300

    Install {obfs,fte}proxy via pip

--- lepidopter-fh/setup-ooniprobe.sh
+++ lepidopter-fh/setup-ooniprobe.sh
@@ -13,17 +13,16 @@ apt-get update
 # Install ooniprobe and pluggable transports dependencies
 apt-get -y install openssl libssl-dev libyaml-dev libffi-dev libpcap-dev tor \
     libgeoip-dev libdumbnet-dev python-dev python-pip
-# Install golang-go obfs4proxy obfsproxy and fteproxy
-# Package obfs4proxy introduces a lite version of meek
-apt-get -y install -t stretch golang-go obfs4proxy obfsproxy fteproxy
+# Install golang-go obfs4proxy includes a lite version of meek
+apt-get -y install -t stretch golang-go obfs4proxy
 
 # Show go version during build-up
 go version
 
 # Remove previous system versions of pyasn1 and python-cryptography
 apt-get -y remove python-pyasn1 python-cryptography
-# Install ooniprobe
-pip install ooniprobe
+# Install ooniprobe obfsproxy and fteproxy
+pip install ooniprobe obfsproxy fteproxy
 
 # Stop running tor service that can lead to a busy chroot mount
 service tor stop
