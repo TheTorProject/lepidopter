0cf7907ee1dc68310ae00b969f2232fe69281a92
anadahz <andz@torproject.org>
2016-08-07 10:50:47 -0300

    Remove the http_invalid_request_line test from default deck

--- lepidopter-fh/opt/ooni/update-deck.sh
+++ lepidopter-fh/opt/ooni/update-deck.sh
@@ -9,3 +9,8 @@ if [ -n "$OONI_DECKGEN" ]; then
 	echo "#ooniprobe deck update: $(date)" > ${OONI_DECK_CONFIG}
 	echo "OONI_DECK=${OONI_DECKGEN}" >> ${OONI_DECK_CONFIG}
 fi
+
+# Remove http_invalid_request_line test from default deck
+awk '/http_invalid_request_line/{for(x=NR-9;x<=NR+1;x++)d[x];} \
+  {a[NR]=$0} END{for(i=1;i<=NR;i++)if(!(i in d))print a[i]}' ${OONI_DECK} > tmp \
+ && mv tmp ${OONI_DECK}
