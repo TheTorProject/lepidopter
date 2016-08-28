5ba4b9852b1f2df62f6b51aaa6aee82dd961e177
anadahz <andz@torproject.org>
2016-08-28 16:31:45 -0300

    Generate SHA message digest for img.xz/zip files only

--- scripts/lepidopter-sign.sh
+++ scripts/lepidopter-sign.sh
@@ -1,17 +1,16 @@
-#!/bin/sh
-set -e
+#!/bin/bash -e
 
 DIR="images"
 LEPIDOPTER_SIGN_KEY="0xBA56AC5A53E9C7A4"
 
 echo "Compute SHA message digests..."
 cd $DIR
-sha1sum *.xz *.zip > SHA1SUM
-sha256sum *.xz *.zip > SHA256SUM
-sha512sum *.xz *.zip > SHA512SUM
+sha1sum *.img.{xz,zip} > SHA1SUM
+sha256sum *.img.{xz,zip} > SHA256SUM
+sha512sum *.img.{xz,zip} > SHA512SUM
 
 echo "Signing..."
-for f in * ; do
+for f in *.img.{xz,zip} SHA*SUM ; do
     gpg2 --verbose --armor --default-key ${LEPIDOPTER_SIGN_KEY} --detach-sign $f
 done
 
