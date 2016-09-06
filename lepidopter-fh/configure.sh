94118e9517399b9e7195810aaacec0a653b62e75
anadahz <andz@torproject.org>
2016-09-06 18:36:22 -0300

    Add lepidopter-update, move ooniprobe.conf to /etc
    * Add lepidopter-update files and set systemd service
    * Add logrotate configuration for lepidopter-update log
    * Set lepidopter-update version file

--- lepidopter-fh/configure.sh
+++ lepidopter-fh/configure.sh
@@ -3,3 +3,10 @@ set -ex
 
 # Set locatime to UTC
 cp /usr/share/zoneinfo/UTC /etc/localtime
+
+# Checkout lepidopter-update repo
+git clone https://github.com/OpenObservatory/lepidopter-update \
+    /opt/ooni/lepidopter-update
+
+# Enable lepidopter-update systemd service
+systemctl enable lepidopter-update
